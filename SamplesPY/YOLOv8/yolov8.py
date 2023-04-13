4eccda12ee30e76efce7ef9954df9b2eae7782ec
HWIGON KIM <hellohello@soynet.io>
2023-04-13 16:11:21 +0900

    fixed bug

--- SamplesPY/YOLOv8/yolov8.py
+++ SamplesPY/YOLOv8/yolov8.py
@@ -26,7 +26,6 @@ model_name = "yolov8s"
 cfg_file = soynet_home + "/mgmt/configs/%s.cfg"%model_name
 engine_file = soynet_home + "/mgmt/engines/%s.bin" % model_name
 weight_file = soynet_home + "/mgmt/weights/%s.weights" % model_name
-plugin  = "plugins"
 
 class_count = len(coco_label)
 conf_thres=0.25
